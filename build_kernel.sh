830ee1b689c6f27503e2124249f0cad411a2eb84
tu4wkl <pachadomenic2010@gmail.com>
2023-10-07 23:28:29 +0300

    Add oc and Tuki names

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,8 +4,8 @@ set -e
 [ ! -e "scripts/packaging/pack.sh" ] && git submodule init && git submodule update
 [ ! -d "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
 
-export KBUILD_BUILD_USER=Royna
-export KBUILD_BUILD_HOST=GrassLand
+export KBUILD_BUILD_USER=Tuki
+export KBUILD_BUILD_HOST=TukiLand
 
 PATH=$PWD/toolchain/bin:$PATH
 
@@ -20,9 +20,9 @@ CONFIG_KSU=vendor/ksu.config
 fi
 
 if [ -z "$DEVICE" ]; then
-export DEVICE=a51
+export DEVICE=m31
 fi
-
+echo "Starting The magic"
 rm -rf out
 make O=out CROSS_COMPILE=aarch64-linux-gnu- CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar \
 	OBJDUMP=llvm-objdump READELF=llvm-readelf -j$(nproc) \
